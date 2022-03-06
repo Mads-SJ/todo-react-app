fda166bde04d39a317d3abdcb1e775281c9f1923
Mads-SJ <mads.soenderstrup@gmail.com>
2022-03-06 15:02:03 +0100

    first commit

---
+++ src/components/TodoForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Button, TextField } from "@material-ui/core";
+
+function TodoForm({ addTodo }) {
+  const [todo, setTodo] = useState({
+    id: "",
+    task: "",
+    completed: false,
+  });
+
+  function handleTaskInputChange(e) {
+    setTodo({ ...todo, task: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (todo.task.trim()) {
+      const id = Math.random().toString(16).slice(2);
+      addTodo({ ...todo, id: id });
+      setTodo({ ...todo, task: "" });
+    }
+  }
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <TextField
+        label="Task"
+        type="text"
+        name="task"
+        value={todo.task}
+        onChange={handleTaskInputChange}
+      />
+      <Button type="submit">submit</Button>
+    </form>
+  );
+}
+
+export default TodoForm;
